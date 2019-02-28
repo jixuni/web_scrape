638219cc92ab8dbda9ba2c60d4e72f343508ad54
Jixu Ni <jixuni@Jixus-MBP.home>
2019-02-28 16:04:09 -0500

    db issues

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,17 @@
 
 
 $(document).ready(function(){
-    $("#webScrape").click(async function(){
-        try{
-             await $.ajax({
-                type: "GET",
-                url: "/scrape"
-            });
-            window.location = "/scrape"
-        } catch(error){
-            console.log(error);
-        }
-    })
+    // $("#webScrape").click(async function(){
+    //     try{
+    //          await $.ajax({
+    //             type: "GET",
+    //             url: "/scrape"
+    //         });
+    //         window.location = "/scrape"
+    //     } catch(error){
+    //         console.log(error);
+    //     }
+    // })
     
 
 })
