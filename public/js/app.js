269d6e31a0d762aca3c3e17cacfd9f9b36477190
Jixu Ni <jixuni@Jixus-MBP.home>
2019-04-11 12:50:04 -0400

    added delete all

--- public/js/app.js
+++ public/js/app.js
@@ -1,30 +1,38 @@
+$(document).ready(function() {
+  $("#webScrape").on("click", async function() {
+    try {
+      await $.ajax({
+        type: "GET",
+        url: "/scrape"
+      });
+      window.location = "/scrape";
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
+  $("#dropData").on("click", async () => {
+    try {
+      await $.ajax({
+        type: "DELETE",
+        url: "dropdata"
+      }).then(location.reload());
+    } catch (error) {
+      console.error(error);
+    }
+  });
 
-$(document).ready(function(){
-    $("#webScrape").on("click", async function(){
-        
-        try{
-             await $.ajax({
-                type: "GET",
-                url: "/scrape"
-            });
-            window.location = "/scrape"
-            
-        } catch(error){
-            console.log(error);
-        }
-    });
-    $(".delPost").on("click", async function() {
-        let id = $(this).data("id");
-        
-        try {
-            await $.ajax({
-                type: "DELETE",
-                url: "/jobs/" + id
-            });
-            window.location.reload();
-        } catch (error) {
-            console.log(error);
-        }
-    });
-})
+  $(".delPost").on("click", async function() {
+    let id = $(this).data("id");
+
+    try {
+      await $.ajax({
+        type: "DELETE",
+        url: "/jobs/" + id
+      });
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  });
+});
--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 
 const exphbs = require("express-handlebars");
-mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/webscrape",
-  );
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/webscrape", {
+  useNewUrlParser: true
+});
 //Initializing axios for our http request
 //using cheerio to parse through DOM
-
 
 const PORT = process.env.PORT || 8080;
 
@@ -20,17 +19,18 @@ const app = express();
 app.use(morgan("dev"));
 
 //Parse request body as JSON
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: true
-}));
+  })
+);
 app.use(express.json());
 // State Folder
 app.use(express.static("public"));
 
 //Telling handlebarjs to render as default page
-app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
 
 //Require all db models
 let db = require("./models");
@@ -39,7 +39,6 @@ let routes = require("./routes/data_controllers");
 
 app.use(routes);
 
-
 app.listen(PORT, function() {
-    console.log(`App running on port ${PORT} !`)
-})
+  console.log(`App running on port ${PORT} !`);
+});
